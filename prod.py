4c5b81269747ff1a05d5ee641048e034a8cb924c
stephen masih <stephenmasih218@gmail.com>
2020-08-20 12:18:49 +0800

    Add files via upload
    Revised fault tolerant python script

--- prod.py
+++ prod.py
@@ -11,19 +11,23 @@ p = KafkaProducer(bootstrap_servers="pkc-43n10.us-central1.gcp.confluent.cloud:9
                   sasl_plain_password="HGVeVIlZQTMuAvMeZXpOhmXpiVwoA77uGlV10kptwtJLrbPxpenbT3MPmYNxmSPi"
                   )
 
-#DWM = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
-serialString = "8d8d,F1,8.34,7.5,1.2"
+DWM = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
+value = ""
 try:
     while True:
-        data = serialString.split(',')
-        k = str.encode(data[0])
-        value = str.encode(json.dumps({"UID": data[0], "FLOOR": data[1], "X": data[2],"Y": data[3],"Z": data[4]}))
-        p.send("data", key=k, value=value)
-        print(k)
-        print(value)
-        time.sleep(0.2)
+        if DWM.in_waiting > 0:
+            value = DWM.readline()
+            if value:
+                data = value.decode(errors='ignore').split(",")
+                if len(data) == 5:
+                    k = str.encode(data[0])
+                    value = str.encode(json.dumps({"UID" : data[0],"FLOOR" : data[1],"X" : float(data[2]),"Y" : float(data[3]),"Z" : float(data[4])}))
+                    p.send("data", key=k, value=value)
+                    print(k)
+                    print(value)
+                    #time.sleep(0.2)
 
 except KeyboardInterrupt as ex:
-    DWM.write("\r".encode())
+    #DWM.write("\r".encode())
     print('keyboard interrupt')
     sys.exit(0)
